1aafa9c916fbe21fac74a22408319f7f2fd8f171
Manling (Jenny) Zhao <jennyzhao0711@gmail.com>
2024-12-20 18:12:05 -0800

    backend structure setup

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+// const authRouter = require("./routes/authRoutes");
+// const productRouter = require("./routes/productRoutes");
+// const cartRouter = require("./routes/cartRoutes");
+const errorHandlerMiddleware = require('./middleware/errorHandler');
+const connectDB = require("./config/db");
+const app = express();
+const port = 4000;
+
+connectDB();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// app.use("/auth", authRouter);
+// app.use("/products", productRouter);
+// app.use("/cart", cartRouter);
+
+app.use(errorHandlerMiddleware);
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
