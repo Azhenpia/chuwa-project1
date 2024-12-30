f16ec927c7501c837140afc6413fad28fe0aa6f5
KismetMaster <35224036+KismetMaster@users.noreply.github.com>
2024-12-30 22:01:04 +0800

    Update ProductModel.js

---
+++ backend/models/ProductModel.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+// Product Schema
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  stock: { // Previously named 'quantity'
+    type: Number,
+    required: true,
+  },
+  imgUrl: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Export Product Model
+module.exports = mongoose.model("Product", productSchema);
