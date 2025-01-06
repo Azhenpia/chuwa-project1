a818a4d232a8061b6012a140e6c31a95949661fa
Manling (Jenny) Zhao <jennyzhao0711@gmail.com>
2025-01-06 12:42:21 -0800

    feat: custom edit btn

---
+++ frontend/src/components/EditBtn.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Button, styled } from "@mui/material";
+
+const CustomEditBtn = styled(Button)(({height, width, fontSize, fontWeight}) => ({
+  height: height || "26px",
+  width: width || "auto",
+  fontSize: fontSize || "10px",
+  fontWeight: fontWeight || "600",
+  color: "#535353",
+  textTransform: "none",
+  borderColor: "#CCCCCC",
+}))
+
+export default function EditBtn({height, width, fontSize, fontWeight, text, onClick}) {
+  return (
+    <CustomEditBtn variant="outlined" height={height} width={width} fontSize={fontSize} onClick={onClick}>{text}</CustomEditBtn>
+  )
+}
