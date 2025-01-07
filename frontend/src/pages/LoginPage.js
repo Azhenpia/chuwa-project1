ac0d83d7531f956df6375e74f2d6deae87b97c2e
Manling (Jenny) Zhao <jennyzhao0711@gmail.com>
2025-01-06 18:45:49 -0800

    feat: login page ui

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,15 +1,37 @@
 import React from 'react';
-import CustomButton from '../components/FilledBtn';
+import FilledBtn from '../components/FilledBtn';
 import InputField from '../components/InputField';
-import OutlinedBtn from '../components/EditBtn';
+import { Link } from 'react-router-dom';
+import '../styles/AuthForm.css';
 
 export default function LoginPage() {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("Submitted")
+  }
+
   return (
-    <div>
-      Login Page
-      <CustomButton text="Sign In"/>
-      <InputField fontSize="16px" error="true" helperText="Invalid Email"/>
-      <OutlinedBtn text="Edit"/>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <h1>Sign in to your account</h1> 
+      <div className="input-field">
+        <label>Email</label>
+        <InputField />
+      </div>
+      <div className="input-field">
+        <label>Password</label>
+        <InputField isPassword={true}/>
+      </div>
+      <div className="submit-btn">
+        <FilledBtn text="Sign In" width="100%" type="submit"/>
+      </div>
+      <div className="footer">
+        <span>
+          Don't have an account? <Link to="/signup">Sign Up</Link>
+        </span>
+        <span>
+          <Link to="/update-password">Forgot Password?</Link>
+        </span>
+      </div>
+    </form>
   )
 }
