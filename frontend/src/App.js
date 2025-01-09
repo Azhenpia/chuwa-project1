8a55418d81699f663cbce86facbca2f3599d75d3
Manling (Jenny) Zhao <jennyzhao0711@gmail.com>
2025-01-08 17:52:04 -0800

    feat: update password ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
+import UpdatePasswordPage from "./pages/UpdatePasswordPage";
+import PasswordSentPage from "./pages/PasswordSentPage";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         {/* <Route path="/" element={<Home />} /> */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/update-password" element={<UpdatePasswordPage />} />
+        <Route path="/update-password/sent" element={<PasswordSentPage />} />
       </Routes>
     </Router>
   );
