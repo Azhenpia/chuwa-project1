0aee0338261b56a37b4e4c4a10560a967edbf76a
Azhenpia <291753162@qq.com>
2025-01-09 16:55:03 -0600

    Enable cors in backend

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,23 @@
-const express = require("express");
-const authRouter = require("./routes/authRoutes");
-const productRouter = require("./routes/productRoutes");
-const cartRouter = require("./routes/cartRoutes");
+const express = require('express');
+const authRouter = require('./routes/authRoutes');
+const productRouter = require('./routes/productRoutes');
+const cartRouter = require('./routes/cartRoutes');
 const errorHandlerMiddleware = require('./middleware/errorHandler');
-const connectDB = require("./config/db");
+const connectDB = require('./config/db');
+var cors = require('cors');
 const app = express();
 const port = 4000;
 
 connectDB();
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(cors());
 
-app.use("/auth", authRouter);
+app.use('/auth', authRouter);
 // app.use("/products", productRouter);
-app.use("/cart", cartRouter);
-app.use("/products", productRouter); // 注册产品路由
+app.use('/cart', cartRouter);
+app.use('/products', productRouter); // 注册产品路由
 app.use(errorHandlerMiddleware);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
