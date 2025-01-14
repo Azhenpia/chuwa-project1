51569e72fb2e789cbc0a2f8e6f8f019c19b41fb7
Manling (Jenny) Zhao <jennyzhao0711@gmail.com>
2025-01-13 17:48:01 -0800

    auth and product forms rwd css

--- frontend/src/pages/Product/CreateProductPage.js
+++ frontend/src/pages/Product/CreateProductPage.js
@@ -139,159 +139,157 @@ export default function CreateProductPage() {
     </div>
   ) : (
     <div className="create-product-page">
-      <div className="product-form">
-        <h1>Create Product</h1>
-        <form onSubmit={handleSubmit}>
+      <h1>Create Product</h1>
+      <form className="product-form" onSubmit={handleSubmit}>
+        <div className="info-input">
+          <label>Product Name</label>
+          <InputField
+            value={formData.name}
+            onChange={handleChange("name")}
+            error={errors.name.hasError}
+            helperText={errors.name.errContent}
+          />
+        </div>
+        <div className="info-input">
+          <label>Product Description</label>
+          <TextField
+            multiline
+            rows={6}
+            variant="outlined"
+            fullWidth
+            value={formData.description}
+            onChange={handleChange("description")}
+            error={errors.description.hasError}
+            helperText={errors.description.errContent}
+            InputProps={{
+              style: { fontSize: "16px" },
+            }}
+            FormHelperTextProps={{
+              style: { fontSize: "14px", textAlign: "right" },
+            }}
+          />
+        </div>
+        <div className="category-price">
           <div className="info-input">
-            <label>Product Name</label>
-            <InputField
-              value={formData.name}
-              onChange={handleChange("name")}
-              error={errors.name.hasError}
-              helperText={errors.name.errContent}
-            />
+            <label>Category</label>
+            <FormControl
+              fullWidth
+              variant="outlined"
+              error={errors.category.hasError}
+            >
+              <Select
+                labelId="category-label"
+                value={formData.category}
+                onChange={handleChange("category")}
+                input={<OutlinedInput />}
+              >
+                <MenuItem value="electronics">Electronics</MenuItem>
+                <MenuItem value="pets">Pets</MenuItem>
+                <MenuItem value="clothing">Clothing</MenuItem>
+              </Select>
+              {errors.category.hasError && (
+                <FormHelperText
+                  style={{ fontSize: "14px", textAlign: "right" }}
+                >
+                  {errors.category.errContent}
+                </FormHelperText>
+              )}
+            </FormControl>
           </div>
           <div className="info-input">
-            <label>Product Description</label>
+            <label>Price</label>
+            <InputField
+              value={formData.price}
+              onChange={handleChange("price")}
+              error={errors.price.hasError}
+              helperText={errors.price.errContent}
+            />
+          </div>
+        </div>
+        <div className="stock-img">
+          <div className="info-input stock">
+            <label>In Stock Quantity</label>
+            <InputField
+              value={formData.stock}
+              onChange={handleChange("stock")}
+              error={errors.stock.hasError}
+              helperText={errors.stock.errContent}
+            />
+          </div>
+          <div className="info-input image">
+            <label>Add Image Link</label>
             <TextField
-              multiline
-              rows={6}
               variant="outlined"
+              disabled
               fullWidth
-              value={formData.description}
-              onChange={handleChange("description")}
-              error={errors.description.hasError}
-              helperText={errors.description.errContent}
+              value={formData.imgUrl}
+              onChange={handleChange("imgUrl")}
+              error={errors.imgUrl.hasError}
+              helperText={errors.imgUrl.errContent}
               InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end" sx={{ marginTop: "10px" }}>
+                    <Button
+                      component="label"
+                      role={undefined}
+                      variant="contained"
+                      sx={{
+                        width: "90px",
+                        height: "40px",
+                        margin: 0,
+                        fontSize: "14px",
+                        textTransform: "none",
+                        backgroundColor: "#5048E5",
+                        color: "white !important",
+                      }}
+                    >
+                      Upload
+                      <VisuallyHiddenInput
+                        type="file"
+                        accept="image/*"
+                        onChange={handleImageUpload}
+                      />
+                    </Button>
+                  </InputAdornment>
+                ),
                 style: { fontSize: "16px" },
               }}
               FormHelperTextProps={{
                 style: { fontSize: "14px", textAlign: "right" },
               }}
+              sx={{
+                ...(errors.imgUrl.hasError && {
+                  "& fieldset": {
+                    borderColor: "#d4342c !important",
+                  },
+                }),
+              }}
             />
           </div>
-          <div className="category-price">
-            <div className="info-input">
-              <label>Category</label>
-              <FormControl
-                fullWidth
-                variant="outlined"
-                error={errors.category.hasError}
-              >
-                <Select
-                  labelId="category-label"
-                  value={formData.category}
-                  onChange={handleChange("category")}
-                  input={<OutlinedInput />}
-                >
-                  <MenuItem value="electronics">Electronics</MenuItem>
-                  <MenuItem value="pets">Pets</MenuItem>
-                  <MenuItem value="clothing">Clothing</MenuItem>
-                </Select>
-                {errors.category.hasError && (
-                  <FormHelperText
-                    style={{ fontSize: "14px", textAlign: "right" }}
-                  >
-                    {errors.category.errContent}
-                  </FormHelperText>
-                )}
-              </FormControl>
-            </div>
-            <div className="info-input">
-              <label>Price</label>
-              <InputField
-                value={formData.price}
-                onChange={handleChange("price")}
-                error={errors.price.hasError}
-                helperText={errors.price.errContent}
-              />
-            </div>
-          </div>
-          <div className="stock-img">
-            <div className="info-input stock">
-              <label>In Stock Quantity</label>
-              <InputField
-                value={formData.stock}
-                onChange={handleChange("stock")}
-                error={errors.stock.hasError}
-                helperText={errors.stock.errContent}
-              />
-            </div>
-            <div className="info-input image">
-              <label>Add Image Link</label>
-              <TextField
-                variant="outlined"
-                disabled
-                fullWidth
-                value={formData.imgUrl}
-                onChange={handleChange("imgUrl")}
-                error={errors.imgUrl.hasError}
-                helperText={errors.imgUrl.errContent}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end" sx={{ marginTop: "10px" }}>
-                      <Button
-                        component="label"
-                        role={undefined}
-                        variant="contained"
-                        sx={{
-                          width: "90px",
-                          height: "40px",
-                          margin: 0,
-                          fontSize: "14px",
-                          textTransform: "none",
-                          backgroundColor: "#5048E5",
-                          color: "white !important",
-                        }}
-                      >
-                        Upload
-                        <VisuallyHiddenInput
-                          type="file"
-                          accept="image/*"
-                          onChange={handleImageUpload}
-                        />
-                      </Button>
-                    </InputAdornment>
-                  ),
-                  style: { fontSize: "16px" },
-                }}
-                FormHelperTextProps={{
-                  style: { fontSize: "14px", textAlign: "right" },
-                }}
-                sx={{
-                  ...(errors.imgUrl.hasError && {
-                    "& fieldset": {
-                      borderColor: "#d4342c !important",
-                    },
-                  }),
-                }}
-              />
-            </div>
-          </div>
-          <div className="img-preview">
-            {imagePreview ? (
-              <img
-                src={imagePreview}
-                alt="Preview"
-                style={{ maxWidth: "100%", maxHeight: "200px" }}
-              />
-            ) : (
-              <>
-                <ImageIcon style={{ color: "#E5E5E5", fontSize: "50px" }} />
-                <p>Image Preview</p>
-              </>
-            )}
-          </div>
-          <div className="submit-btn">
-            <FilledBtn
-              text={isLoading ? <CircularProgress size={20} /> : "Add Product"}
-              width="150px"
-              type="submit"
-              disabled={isLoading}
+        </div>
+        <div className="img-preview">
+          {imagePreview ? (
+            <img
+              src={imagePreview}
+              alt="Preview"
+              style={{ maxWidth: "100%", maxHeight: "200px" }}
             />
-          </div>
-        </form>
-      </div>
+          ) : (
+            <>
+              <ImageIcon style={{ color: "#E5E5E5", fontSize: "50px" }} />
+              <p>Image Preview</p>
+            </>
+          )}
+        </div>
+        <div className="submit-btn">
+          <FilledBtn
+            text={isLoading ? <CircularProgress size={20} /> : "Add Product"}
+            width="150px"
+            type="submit"
+            disabled={isLoading}
+          />
+        </div>
+      </form>
     </div>
   );
 }
